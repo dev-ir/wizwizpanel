b47e251a0d2179af746543fa9d54bc5ab4e5b4a5
unknown <info@developermen.ir>
2024-07-13 16:26:24 +0330

    Add Description for verify User

--- settings.php
+++ settings.php
@@ -676,9 +676,10 @@ if (file_exists($lang_file)) {
                     <div class="grid grid-cols-1 gap-6 mt-4 xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 text-left" style="font-size: 14px">
                         <div class="mt-2">
                             <label class="text-gray-700 dark:text-gray-200 mt-2" for="verify_user"><?php echo $_LANG['verify_user']?></label>
-                            <input required name="verify_user" value="<?php echo @$value["verify_user"]; ?>" id="verify_user"
-                                   type="text"
-                                   class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                            <input required name="verify_user" value="<?php echo @$value["verify_user"]; ?>" id="verify_user" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                            <h6 class="text-white">
+                                در این حالت کاربران جهت مشاهده اطلاعات پرداخت میبایست کد معرف دریافت کنند.
+                            </h6>
                         </div>
                     </div>
                     <div class="flex justify-end mt-6">
